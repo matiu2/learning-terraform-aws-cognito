721e33f40f0394a4b3b18cd0323f708bd89cc50b
Matthew Sherborne <msherborne@gmail.com>
2021-06-15 21:04:17 +1000

    Should have added this ages ago

---
+++ private_bucket.tf
@@ -0,0 +1,32 @@
+locals {
+    private_bucket_name = "${local.bucket_name}-private"
+}
+
+resource "aws_s3_bucket" "private_bucket" {
+  bucket = local.private_bucket_name
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["http://${aws_s3_bucket.app_bucket.website_endpoint}"]
+  }
+}
+
+// Copy the patch objects to our bucket
+
+data "aws_s3_bucket_objects" "patches" {
+  bucket = var.source_bucket
+  prefix = var.source_patches_prefix
+}
+
+locals {
+  // Remove the prefix from the files, and remove the 'just the prefix' from the list of files
+  prefix_files = compact([for key in data.aws_s3_bucket_objects.patches.keys: trimprefix(key, "${data.aws_s3_bucket_objects.patches.prefix}/")])
+}
+
+resource "aws_s3_object_copy" "patches" {
+  // Add the prefix back again
+  for_each = {for postfix in local.prefix_files: "${var.source_bucket}/${data.aws_s3_bucket_objects.patches.prefix}/${postfix}" => postfix}
+  bucket = aws_s3_bucket.private_bucket.bucket
+  key    = each.value
+  source = each.key
+}
