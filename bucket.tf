c5e8fae99c53c5490d7e40dbec94685a2da1f5ab
Matthew Sherborne <msherborne@gmail.com>
2021-06-15 14:33:15 +1000

    Now copies the objects to the new bucket

--- bucket.tf
+++ bucket.tf
@@ -8,14 +8,30 @@ resource "aws_s3_bucket" "private_bucket" {
   }
 }
 
-resource "aws_s3_object_copy" "serverless_app" {
-  bucket = aws_s3_bucket.private_bucket
-  key    = "destination_key"
-  source = "source_bucket/source_key"
+// Copy from cl-labs-s3content/aws-cognito-web-identity-federation/appbucket to our bucket
 
-  grant {
-    uri         = "http://acs.amazonaws.com/groups/global/AllUsers"
-    type        = "Group"
-    permissions = ["READ"]
-  }
+data "aws_s3_bucket_objects" "app" {
+  bucket = var.source_bucket
+  prefix = var.source_app_prefix
+}
+
+resource "aws_s3_object_copy" "app" {
+  count  = length(data.aws_s3_bucket_objects.app.keys)
+  bucket = aws_s3_bucket.private_bucket.bucket
+  key    = var.source_app_prefix
+  source = "${var.source_bucket}/${data.aws_s3_bucket_objects.app.keys[count.index]}"
+}
+
+// Copy from cl-labs-s3content/aws-cognito-web-identity-federation/patches to our bucket
+
+data "aws_s3_bucket_objects" "patches" {
+  bucket = var.source_bucket
+  prefix = var.source_patches_prefix
+}
+
+resource "aws_s3_object_copy" "patches" {
+  count  = length(data.aws_s3_bucket_objects.app.keys)
+  bucket = aws_s3_bucket.private_bucket.bucket
+  key    = var.source_patches_prefix
+  source = "${var.source_bucket}/${data.aws_s3_bucket_objects.patches.keys[count.index]}"
 }
--- variables.tf
+++ variables.tf
@@ -4,16 +4,22 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "source-bucket-name" {
+variable "source_bucket" {
   description = "Bucket where we'll get our lab content from"
   type        = string
   default     = "cl-labs-s3content"
 }
 
-variable "source-bucket-prefix" {
+variable "source_app_prefix" {
   description = "Directory where we'll get our lab content from"
   type        = string
   default     = "aws-cognito-web-identity-federation/appbucket"
+}
+
+variable "source_patches_prefix" {
+  description = "Directory where we'll get our lab patches from"
+  type        = string
+  default     = "aws-cognito-web-identity-federation/patches"
 }
 
 variable "destination_bucket" {
